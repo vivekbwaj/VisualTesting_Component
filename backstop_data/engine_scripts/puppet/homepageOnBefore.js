9c34386720f76fafd30bb7d560bbc8c9ade544da
Vivek Bhardwaj <Vivek.Bhardwaj@crownbet.com.au>
2019-02-26 15:24:22 +1100

    beautification

--- backstop_data/engine_scripts/puppet/homepageOnBefore.js
+++ backstop_data/engine_scripts/puppet/homepageOnBefore.js
@@ -1,3 +1,4 @@
+const chalk = require("chalk");
 let ticket_num;
 
 if(process.argv.length>4)
@@ -6,7 +7,7 @@ if(process.argv.length>4)
 }
 
 module.exports = async (page, scenario, viewport) => {
-    console.log("SCENARIO > " + scenario.label+" > "+ viewport.name);
+    console.log(chalk.red.bgRed.bold("SCENARIO > " + scenario.label+" > "+ viewport.name));
 
     const multi = await require("../../mockresponses/multi.json");
     const mu4 = await require("../../mockresponses/multiUpcoming4.json");
@@ -14,7 +15,6 @@ module.exports = async (page, scenario, viewport) => {
     const nba = await require("../../mockresponses/nbaVision37.json");
     const ntj = await require("../../mockresponses/nextToJump123.json");
     const rcToday = await require("../../mockresponses/racingToday.json");
-    const screenshotDir = process.cwd() + "/backstop_data/engine_Scripts_Screenshots/";
     const Mocker = await require("../../../utils/Mocker");
 
     mck = new Mocker(page);
@@ -28,13 +28,11 @@ module.exports = async (page, scenario, viewport) => {
 
     mck.mocker();
     mck.mock(false, "/apiproxy/mobile/event/sports/multi/", multi);
-    // mck.mock(false, "/apiproxy/WebEventAPI/racing/multibet/upcoming/1,2,3/4", mu4);
     mck.mock(false, "/apiproxy/WebEventAPI/racing/multibet/upcoming/1,2,3/8", mu8);
     mck.mock(false, "/apiproxy/nbavision/epg/37", nba);
     mck.mock(false, "/api/home/next-jumps/1,2,3", ntj);
     mck.mock(false, "/api/racing/racing/", rcToday);
     console.log("response mocked for all Apis on homepage");
 
-    await page.screenshot({path: screenshotDir + "homepage.png", fullPage: true});
     await page.setDefaultNavigationTimeout(30000);
 };
--- backstop_data/engine_scripts/puppet/onReady_clickSelector.js
+++ backstop_data/engine_scripts/puppet/onReady_clickSelector.js
@@ -1,7 +1,7 @@
 const clickSelectors = require("../engineUtils/custom_ClickSelectors.js");
 
 module.exports = async (page, scenario,viewport) => {
-    console.log("SCENARIO > " + scenario.label+" > "+ viewport.name);
+    // console.log("SCENARIO > " + scenario.label+" > "+ viewport.name);
     await require("./clickAndHoverHelper")(page, scenario);
     await clickSelectors(scenario, page,viewport);
 };
